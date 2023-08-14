def9be6255f259b79a6a58f262b465f00e137087
zejba <66516617+zejba@users.noreply.github.com>
2023-08-15 01:32:47 +0900

    add servant db beta

--- js/addform.js
+++ js/addform.js
@@ -136,15 +136,16 @@ function npCalcCheck(p) {
   }
 }
 
+//クラススコア自動入力
 function addScoreForm(p) {
-  scoreType = ["b_power_buff","b_cr_buff","a_power_buff","a_cr_buff","q_power_buff","q_cr_buff","ex_buff","cr_buff","np_buff"];
-  scoreAmount = [20,20,20,40,20,60,50,10,10];
-  for (let i = 0; i < 9; i++) {
+  scoreType = ["b_power_buff","b_cr_buff","a_power_buff","a_cr_buff","q_power_buff","q_cr_buff","ex_buff","cr_buff","np_buff","starget_buff"];
+  scoreAmount = [20,20,20,40,20,60,50,10,10,50];
+  for (let i = 0; i < 10; i++) {
     addForm(p);
   }
   let buffForms = p.parentElement.getElementsByClassName("buff-form");
-  let x = buffForms.length - 9;
-  for (let i = 0; i < 9; i++) {
+  let x = buffForms.length - 10;
+  for (let i = 0; i < 10; i++) {
     buffForms[x+i].getElementsByClassName('skill-name')[0].value = "スコア";
     buffForms[x+i].getElementsByClassName('skill-type')[0].value = scoreType[i];
     buffForms[x+i].getElementsByClassName('amount')[0].value = scoreAmount[i];
--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 function openMenu(p) {
-    document.querySelector('.menu').classList.toggle('is-active');
+  document.querySelector('.menu').classList.toggle('is-active');
 }
+
+//サーヴァントリストを生成
+function generateServantList() {
+  let csv = new XMLHttpRequest();
+  csv.open("GET", "data/servant_data.csv",false);
+  csv.send(null);
+  if (csv.status != 200) {
+    return;
+  }
+  let arr = [];
+  let list = csv.responseText.split('\n');
+  for (let i = 0; i < list.length; i++) {
+    arr.push(list[i].split(',').slice(0,2));
+  }
+
+  let menu = document.getElementById("servant-list");
+  for(var i=0;i<arr.length;i++){
+    let opt = document.createElement("option");
+    opt.value = arr[i][0];
+    opt.text = arr[i][0] + "." + arr[i][1];
+    menu.appendChild(opt);
+  }
+}
+  
