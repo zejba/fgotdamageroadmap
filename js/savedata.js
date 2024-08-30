1a91a4ecc84e87a0d850434e63bb36c19f33f469
zejba <66516617+zejba@users.noreply.github.com>
2024-08-31 03:33:00 +0900

    update 20240831

--- js/savedata.js
+++ js/savedata.js
@@ -185,7 +185,7 @@ function inputSkillData(turn,card,arrc) {
 //データベース
 function reflectsvData() {
   let csv = new XMLHttpRequest();
-  csv.open("GET", "data/servant_data.csv?date=202408122230",false);
+  csv.open("GET", "data/servant_data.csv?date=202408310330",false);
   csv.send(null);
   if (csv.status != 200) {
     return;
@@ -246,7 +246,7 @@ function reflectsvData() {
 
 function reflectQData() {
   let csv = new XMLHttpRequest();
-  csv.open("GET", "data/quest_data.csv?date=202408122230",false);
+  csv.open("GET", "data/quest_data.csv?date=202408310330",false);
   csv.send(null);
   if (csv.status != 200) {
     return;
